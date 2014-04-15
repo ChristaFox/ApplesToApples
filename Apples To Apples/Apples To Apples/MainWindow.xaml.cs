64b240e627820ee72ae2a855878b414538284738
crasmu75 <crasmu75@bruinmail.slcc.edu>
2014-04-15 16:22:11 -0600

    Start of the GUI

---
+++ Apples To Apples/Apples To Apples/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Apples_To_Apples
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public Boolean IsJudge()
+        {
+            return false;
+        }
+
+        private void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+            StartPage.Visibility = System.Windows.Visibility.Collapsed;
+            LblTitle.Visibility = System.Windows.Visibility.Collapsed;
+            if (IsJudge())
+                JudgeView.Visibility = System.Windows.Visibility.Visible;
+            else
+                PlayerView.Visibility = System.Windows.Visibility.Visible; 
+        }
+    }
+}
